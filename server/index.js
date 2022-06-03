e58336877162a0b56886e5bad0a34eb14aad04b8
Leon Antoshyn <Doctorsprait@gmail.com>
2022-06-03 12:26:01 +0300

    router doesn't work

--- server/index.js
+++ server/index.js
@@ -4,6 +4,18 @@ const sequelize = require('./db');
 const models = require('./models/models')
 const PORT = process.env.PORT || 5000;
 const app = express();
+const cors = require('cors');
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
+app.get('/', (req,res) => {
+    res.status(200).json({message: 'OK'});
+})
+app.use(errorHandler)
+
 const start = async () => {
     try{
         await sequelize.authenticate()
